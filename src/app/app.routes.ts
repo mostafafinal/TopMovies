e1d8b36b0c2adfc9bfaed049c973c6fd1463ff5d
Mostafa <mostafafinal55@gmail.com>
2024-09-11 01:04:19 +0300

    Project Initialization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MoviesPageComponent } from './components/movies-page/movies-page.component';
+import { TvShowsPageComponent } from './components/tv-shows-page/tv-shows-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  { path: 'movies', component: MoviesPageComponent },
+  { path: 'tvshows', component: TvShowsPageComponent },
+];
---
+++ src/app/components/tv-shows-page/tv-shows-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-shows-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './tv-shows-page.component.html',
+  styleUrl: './tv-shows-page.component.css'
+})
+export class TvShowsPageComponent {
+
+}
