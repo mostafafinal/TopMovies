0e766aa9580638859f8e7d19538b6d6c9ab7f07d
Abd0bshir <Abdobshir535@gmail.com>
2024-09-12 16:02:16 +0200

    Home page by 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { CardCarouselComponent } from './components/card-carousel/card-carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    CommonModule,
+    CardCarouselComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
