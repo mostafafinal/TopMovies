a00da9f16ebb954bff49b817d3eb93c28121ae23
Abd0bshir <Abdobshir535@gmail.com>
2024-09-13 17:36:37 +0200

    Shof dol

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { MoviesPageComponent } from './components/movies-page/movies-page.component';
 import { TvShowsPageComponent } from './components/tv-shows-page/tv-shows-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
 
 export const routes: Routes = [
   {
@@ -10,4 +12,6 @@ export const routes: Routes = [
   },
   { path: 'movies', component: MoviesPageComponent },
   { path: 'tvshows', component: TvShowsPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
 ];
