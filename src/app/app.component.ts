6d40cf4126cf9889b259442688bfe59869116122
David Essam <davidessam162@gmail.com>
2024-09-13 19:24:14 +0300

    details-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,DetailsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MoviesPageComponent } from './components/movies-page/movies-page.component';
 import { TvShowsPageComponent } from './components/tv-shows-page/tv-shows-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
 
 export const routes: Routes = [
   {
@@ -10,4 +11,5 @@ export const routes: Routes = [
   },
   { path: 'movies', component: MoviesPageComponent },
   { path: 'tvshows', component: TvShowsPageComponent },
+  { path: 'detailspage', component:DetailsPageComponent },
 ];
