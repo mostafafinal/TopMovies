596694663c458e098044bcd856ac14fc464c30f6
Mostafa <mostafafinal55@gmail.com>
2024-09-13 19:43:26 +0300

    Details Page Updates

--- src/app/components/movies-page/movies-page.component.ts
+++ src/app/components/movies-page/movies-page.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-movies-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './movies-page.component.html',
   styleUrl: './movies-page.component.css',
 })
