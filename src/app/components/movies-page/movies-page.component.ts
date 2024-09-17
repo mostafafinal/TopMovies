c595215b4c69d4c689951e2a68af8a258011bb45
Mostafa Ahmed <mostafafinal55@gmail.com>
2024-09-17 20:26:49 +0300

    Revert "add paignation and fillter"

--- src/app/components/movies-page/movies-page.component.ts
+++ src/app/components/movies-page/movies-page.component.ts
@@ -1,71 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component, TrackByFunction } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AllMoives, Movies } from '../../models/movies';
+import { Movies } from '../../models/movies';
 import { MoviesService } from '../../services/movies.service';
-import { PaginationModule } from '../../models/pagination.module';
-import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-movies-page',
   standalone: true,
-  imports: [CommonModule, RouterModule, PaginationModule, PaginationComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './movies-page.component.html',
-  styleUrls: ['./movies-page.component.css'],
+  styleUrl: './movies-page.component.css',
 })
 export class MoviesPageComponent {
-  AllMoives: Movies[] = []; 
-  filteredMovies: Movies[] = []; 
-  typeMoives: string[] = [];
-  totalMovies: number = 133;
-  currentPage: number = 1;
-  limit: number = 20;
-  trackByMovieId: TrackByFunction<Movies> = (index: number, movie: Movies) => movie.id;
+  allMovies: Movies[] = [];
 
-
+  trackByMovieId(movie: any): number {
+    return movie.id;
+  }
   constructor(private movieService: MoviesService) {}
 
   ngOnInit(): void {
-    this.fetchMovies(this.currentPage);
+    this.getMovies();
   }
-
-  fetchMovies(page: number): void {
-    const startIndex = (page - 1) * this.limit;
-    const endIndex = startIndex + this.limit;
-
-    this.movieService.getmoives().subscribe((data) => {
-      // pagination
-      this.totalMovies = data.movies.length;
-      this.AllMoives = data.movies.slice(startIndex, endIndex);
-      this.filteredMovies = [...this.AllMoives]; 
-      
-     
-      this.AllMoives.forEach((movie) => {
-        let genres = JSON.parse(movie.genres.toString());
-        this.typeMoives.push(...genres); 
-         
-      });
-      console.log(this.filteredMovies)
+  getMovies() {
+    this.movieService.getMovies().subscribe((data) => {
+      this.allMovies = data.movies;
+      console.log(this.allMovies);
     });
-  }
-
-  changePage(page: number): void {
-    this.currentPage = page;
-    this.fetchMovies(page);  
-    
-  }
-
-  
-  filterByGenre(genre: string): void {
-    // console.log("hello")
-    this.filteredMovies = this.AllMoives.filter(movie => {
-      let genres = JSON.parse(movie.genres.toString());
-      return genres.includes(genre);
-    });
-  }
-
-  GetAll()
-  {
-    this.filteredMovies=this.AllMoives
   }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,25 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, throwError } from 'rxjs';
-import { AllMoives, Movies } from '../models/movies';
+import { Observable } from 'rxjs';
 
-
-const options={
-  params:{
-    pages:'1',
-  },
-}
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
-  private apiUrl ='https://movie-app-production-bac6.up.railway.app/movie/movies';
+  private apiUrl =
+    'https://movie-app-production-bac6.up.railway.app/movie/movies';
   constructor(private http: HttpClient) {}
 
- getmoives():Observable<AllMoives>
- {
-return this.http.get<any>(this.apiUrl, options)
-  
- }
-  
+  getMovies(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
