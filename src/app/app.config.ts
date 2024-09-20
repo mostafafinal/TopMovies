975e155b2126c1c9bbb9bec30f7fa08ab08ad69b
Mostafa <mostafafinal55@gmail.com>
2024-09-21 00:48:40 +0300

    Interceptor Update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
+// app.config.ts
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { TokenInterceptor } from './interceptor/token.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
@@ -11,12 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptor,
-      multi: true,
-    },
+    provideHttpClient(withInterceptors([TokenInterceptor])),
     provideAnimations(),
     provideToastr(),
   ],
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,28 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUserData(): Observable<any> {
-    let headers = new HttpHeaders()
-    headers = headers.append('Authorization', `Bearer ${localStorage.getItem('authToken')}`);
-    return this.http.get<any>('https://movie-app-production-bac6.up.railway.app/user/', {headers});
+    // let headers = new HttpHeaders()
+    // headers = headers.append('Authorization', `Bearer ${localStorage.getItem('authToken')}`);
+    return this.http.get<any>(
+      'https://movie-app-production-bac6.up.railway.app/user/'
+    );
   }
   getUserFavList(): Observable<any> {
-    let headers = new HttpHeaders()
-    headers = headers.append('Authorization', `Bearer ${localStorage.getItem('authToken')}`);
- 
-    return this.http.get<any>('https://movie-app-production-bac6.up.railway.app/user/favList',{headers});
+    // let headers = new HttpHeaders();
+    // headers = headers.append(
+    //   'Authorization',
+    //   `Bearer ${localStorage.getItem('authToken')}`
+    // );
+
+    return this.http.get<any>(
+      'https://movie-app-production-bac6.up.railway.app/user/favList'
+    );
   }
   getUserWatchLaterList(): Observable<any> {
-    let headers = new HttpHeaders()
-    headers = headers.append('Authorization', `Bearer ${localStorage.getItem('authToken')}`);
-    return this.http.get<any>('https://movie-app-production-bac6.up.railway.app/user/watctLater',{headers});
+    // let headers = new HttpHeaders();
+    // headers = headers.append(
+    //   'Authorization',
+    //   `Bearer ${localStorage.getItem('authToken')}`
+    // );
+    return this.http.get<any>(
+      'https://movie-app-production-bac6.up.railway.app/user/watctLater'
+    );
   }
 }
