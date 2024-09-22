c3108782ac982b9a5ba31705d01a6b1f63f58a5a
Mostafa <mostafafinal55@gmail.com>
2024-09-22 18:40:50 +0300

    Signup Error & Functionality Handling

--- src/app/components/signup-page/signup-page.component.ts
+++ src/app/components/signup-page/signup-page.component.ts
@@ -1,60 +1,58 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { RegisterService } from '../../services/register.service';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [RouterModule,FormsModule,ReactiveFormsModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css',
 })
 export class SignupPageComponent {
-  firstName: string='';
-  lastName: string='';
+  firstName: string = '';
+  lastName: string = '';
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
-  errorMessage: string = '';
+  signUpform!: FormGroup;
   constructor(
     private registersService: RegisterService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private toaster: ToastrService,
+    private router: Router
   ) {
-
-    this.signUpform = this.formBuilder.group({
-      firstName: ['', Validators.required],  
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required] // Add the confirmPassword field
-    },  { validators: RegisterService.passwordMatchValidator() }); // Apply the custom validator);
+    this.signUpform = this.formBuilder.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: RegisterService.passwordMatchValidator() }
+    );
   }
-
-  signUpform!: FormGroup;
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    // this.registerUser();
-  }
-  onSubmit(): void {
-    if (this.signUpform.valid ) {
-      this.signUpform.value;
-      // console.log('Form Submitted', this.signUpform.value);
-      // Here you can handle the sign-up logic (e.g., send data to the backend)
-    } else {
-      console.log('Form is invalid');
-    }
-  }
-
 
   registerUser(): void {
-    this.registersService.register(this.firstName,this.lastName, this.email, this.password).subscribe(
-      (response) => {
-        console.log('User registered successfully', response);
-      },
-      (error) => {
-        this.errorMessage = error.message;
-      }
-    );
+    this.registersService
+      .register(this.firstName, this.lastName, this.email, this.password)
+      .subscribe(
+        (response) => {
+          this.toaster.success(response.message, 'Success');
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          this.toaster.error(error.error.message, 'Failed');
+          this.router.navigate(['/login']);
+        }
+      );
   }
 }
